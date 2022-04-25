237f8240580fdecad3af441c127ecee98010b448
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-04-25 19:44:24 +0530

    migrated my bound service class to boundService package.

--- app/src/main/java/boundService/MyBoundService.kt
+++ app/src/main/java/boundService/MyBoundService.kt
@@ -1,4 +1,4 @@
-package com.example.tictactoe
+package boundService
 
 import android.app.Service
 import android.content.Intent
--- app/src/main/java/com/example/tictactoe/MainActivity2.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity2.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import boundService.MyBoundService
 
 class MainActivity2 : AppCompatActivity() {
 
