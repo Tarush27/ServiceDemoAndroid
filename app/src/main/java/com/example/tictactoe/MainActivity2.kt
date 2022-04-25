7b9e1e2e0c5c35a745758c58870260fd54a64f70
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-04-25 19:42:34 +0530

    initialized UI components.

--- app/src/main/java/com/example/tictactoe/MainActivity2.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity2.kt
@@ -1,12 +1,25 @@
 package com.example.tictactoe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
+    private val startBtn: Button = findViewById(R.id.btnStart)
+    private val stopBtn: Button = findViewById(R.id.btnStop)
+    private val getRandomNoBtn: Button = findViewById(R.id.randomNoBtn)
+    private val showRandomNoTextView: TextView = findViewById(R.id.showRandomTextView)
+    private val isRunning: Nothing = TODO()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        startBtn.setOnClickListener {
+            val intent = Intent(this, MyBoundService::class.java)
+        }
     }
 }
