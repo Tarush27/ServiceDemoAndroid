df70878e89702ae24bf4415ebb851b39fb881743
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-05-06 18:08:35 +0530

    added service connection implementation to show working of bound services.

--- app/src/main/java/com/example/tictactoe/MainActivity2.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity2.kt
@@ -1,19 +1,40 @@
 package com.example.tictactoe
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import boundService.MyBoundService
+import kotlin.properties.Delegates
 
 class MainActivity2 : AppCompatActivity() {
 
-    private val startBtn: Button = findViewById(R.id.btnStart)
-    private val stopBtn: Button = findViewById(R.id.btnStop)
-    private val getRandomNoBtn: Button = findViewById(R.id.randomNoBtn)
-    private val showRandomNoTextView: TextView = findViewById(R.id.showRandomTextView)
-    private val isRunning: Nothing = TODO()
+    private lateinit var myBoundService: MyBoundService
+    private val startBtn: Button
+        get() = findViewById(R.id.btnStart)
+    private val stopBtn: Button get() = findViewById(R.id.btnStop)
+    private val getRandomNoBtn: Button get() = findViewById(R.id.randomNoBtn)
+    private val showRandomNoTextView: TextView get() = findViewById(R.id.showRandomTextView)
+    private var isRunning: Boolean = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            val myBinder = p1 as MyBoundService.MyBinder
+            myBoundService = myBinder.getService()
+            isRunning = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isRunning = false
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +42,18 @@ class MainActivity2 : AppCompatActivity() {
 
         startBtn.setOnClickListener {
             val intent = Intent(this, MyBoundService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+        stopBtn.setOnClickListener {
+            unbindService(connection)
+            isRunning = false
+        }
+
+        getRandomNoBtn.setOnClickListener {
+            if (isRunning) {
+                showRandomNoTextView.text = myBoundService.generateIntegerRandomNo().toString()
+            }
         }
     }
 }
+
